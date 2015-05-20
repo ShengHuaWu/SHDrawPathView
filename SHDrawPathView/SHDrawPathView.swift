79bddab50cf141b2199a9509c97ce033242d5943
ShengHua Wu <fantasy0404@gmail.com>
2015-05-20 10:33:08 +0800

    Render image in background.

--- SHDrawPathView/SHDrawPathView.swift
+++ SHDrawPathView/SHDrawPathView.swift
@@ -13,44 +13,48 @@ public class SHDrawPathView: UIView {
     // MARK: Public property
     public var strokeColor: UIColor = UIColor.blueColor() {
         didSet {
-            self.setNeedsDisplay()
+            self.setNeedsLayout()
         }
     }
     public var strokeWidth: CGFloat = 3.0 {
         didSet {
-            self.setNeedsDisplay()
+            self.setNeedsLayout()
         }
     }
     
     // MARK: Private propery
     private let path: UIBezierPath = UIBezierPath()
     private let tolerance: CGFloat = 10
+    private let backgroundImageView: UIImageView
     
     // MARK: Designated initializer
     public override init(frame: CGRect) {
         self.path.lineCapStyle = kCGLineCapRound
         self.path.lineJoinStyle = kCGLineJoinRound
         self.path.lineWidth = self.strokeWidth
+        self.backgroundImageView = UIImageView(frame: CGRectZero)
         
         super.init(frame: frame)
         self.backgroundColor = UIColor.clearColor()
+        self.addSubview(self.backgroundImageView)
     }
     
     public required init(coder aDecoder: NSCoder) {
         self.path.lineCapStyle = kCGLineCapRound
         self.path.lineJoinStyle = kCGLineJoinRound
         self.path.lineWidth = self.strokeWidth
+        self.backgroundImageView = UIImageView(frame: CGRectZero)
         
         super.init(coder: aDecoder)
         self.backgroundColor = UIColor.clearColor()
+        self.addSubview(self.backgroundImageView)
     }
     
-    // MARK: Draw rect
-    public override func drawRect(rect: CGRect) {
-        if !self.path.empty {
-            self.strokeColor.setStroke()
-            self.path.stroke()
-        }
+    // MARK: Layout
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundImageView.frame = self.bounds
+        self.renderImageInBackgroundForRect(self.backgroundImageView.frame)
     }
     
     // MARK: Touch events
@@ -69,13 +73,30 @@ public class SHDrawPathView: UIView {
         let point = touch.locationInView(self)
         if self.shouldDrawPathToPoint(point) {
             self.path.addLineToPoint(point)
-            self.setNeedsDisplay()
+            self.setNeedsLayout()
         }
     }
     
     // MARK: Tolerance
     func shouldDrawPathToPoint(point: CGPoint) -> Bool {
         return self.path.currentPoint.distanceFromPoint(point) >= self.tolerance
+    }
+    
+    // MARK: Render
+    func renderImageInBackgroundForRect(rect: CGRect) {
+        NSOperationQueue().addOperationWithBlock { () -> Void in
+            UIGraphicsBeginImageContext(rect.size)
+            if !self.path.empty {
+                self.strokeColor.setStroke()
+                self.path.stroke()
+            }
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.backgroundImageView.image = image
+            })
+        }
     }
     
 }
